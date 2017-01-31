e24e265f8648ed03fc6bc78a2df8124c756ffdf6
tobihrbr <tobihrbr@gmail.com>
2017-01-31 19:37:37 +0100

    update test

--- test.js
+++ test.js
@@ -1,17 +1,18 @@
 const test = require('./app.js');
 
-test('https://google.com').then(function() {
-  console.log('Google');
-  console.log('ok');
-}).catch(function() {
-  console.log('Google');
-	console.log('no');
-})
-
 test('https://not-a-real-domain-i-guess.com').then(function() {
   console.log('Something else');
   console.log('ok');
 }).catch(function() {
   console.log('Something else');
+	console.log('no');
+})
+
+
+test('https://google.com').then(function() {
+  console.log('Google');
+  console.log('ok');
+}).catch(function() {
+  console.log('Google');
 	console.log('no');
 })
