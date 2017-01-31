afd23d982fd906a223d4d951a08160295a69fb18
tobihrbr <tobihrbr@gmail.com>
2017-01-31 19:37:10 +0100

    update rest

--- test.js
+++ test.js
@@ -1,7 +1,17 @@
 const test = require('./app.js');
 
 test('https://google.com').then(function() {
+  console.log('Google');
   console.log('ok');
 }).catch(function() {
+  console.log('Google');
+	console.log('no');
+})
+
+test('https://not-a-real-domain-i-guess.com').then(function() {
+  console.log('Something else');
+  console.log('ok');
+}).catch(function() {
+  console.log('Something else');
 	console.log('no');
 })
